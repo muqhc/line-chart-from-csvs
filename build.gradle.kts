3ff7384ba97dc4bf2c5b1e8fd5ffefc4960b728a
muqhc <88622655+muqhc@users.noreply.github.com>
2023-07-12 18:02:14 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+// Versions
+val kotlinVersion: String = 1.8.20
+val kvisionVersion: String = 6.6.0
+
+plugins {
+    kotlin("plugin.serialization") version kotlinVersion
+    kotlin("js") version kotlinVersion
+    id("io.kvision") version kvisionVersion
+}
+
+version = "1.0.0-SNAPSHOT"
+group = "io.github.muqhc"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+
+val webDir = file("src/main/web")
+
+kotlin {
+    js {
+        browser {
+            runTask {
+                outputFileName = "main.bundle.js"
+                sourceMaps = false
+                devServer = KotlinWebpackConfig.DevServer(
+                    open = false,
+                    port = 3000,
+                    proxy = mutableMapOf(
+                        "/kv/*" to "http://localhost:8080",
+                        "/kvws/*" to mapOf("target" to "ws://localhost:8080", "ws" to true)
+                    ),
+                    static = mutableListOf("$buildDir/processedResources/js/main")
+                )
+            }
+            webpackTask {
+                outputFileName = "main.bundle.js"
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                }
+            }
+        }
+        binaries.executable()
+    }
+    sourceSets["main"].dependencies {
+        implementation("io.kvision:kvision:$kvisionVersion")
+        implementation("io.kvision:kvision-bootstrap:$kvisionVersion")
+        implementation("io.kvision:kvision-i18n:$kvisionVersion")
+    }
+    sourceSets["test"].dependencies {
+        implementation(kotlin("test-js"))
+        implementation("io.kvision:kvision-testutils:$kvisionVersion")
+    }
+    sourceSets["main"].resources.srcDir(webDir)
+}
