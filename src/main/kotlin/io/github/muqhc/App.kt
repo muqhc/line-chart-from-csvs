08ca27429e5bc842d908456654164951a38ac049
muqhc <muqhc07@gmail.com>
2023-07-13 23:35:20 +0900

    many many many many

--- src/main/kotlin/io/github/muqhc/App.kt
+++ src/main/kotlin/io/github/muqhc/App.kt
@@ -3,14 +3,13 @@ package io.github.muqhc
 import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 import io.kvision.*
 import io.kvision.chart.*
+import io.kvision.chart.js.Point
+import io.kvision.chart.js.ScatterController
+import io.kvision.chart.js.ScatterDataPoint
 import io.kvision.core.*
-import io.kvision.form.upload.upload
-import io.kvision.form.upload.uploadInput
 import io.kvision.html.*
 import io.kvision.panel.root
-import io.kvision.types.contentType
-import io.kvision.utils.getContent
-import org.w3c.files.Blob
+import io.kvision.panel.vPanel
 import org.w3c.files.FileReader
 
 class App : Application() {
@@ -19,77 +18,121 @@ class App : Application() {
         val chartId = "myChart"
 
         val chartColorList = listOf(
-            Color.name(Col.GRAY),
-            Color.name(Col.RED),
-            Color.name(Col.PINK),
-            Color.name(Col.BLUE),
-            Color.name(Col.AQUA),
-        )
+            Col.BLUE,
+            Col.RED,
+            Col.BROWN,
+            Col.HOTPINK,
+            Col.CYAN,
+            Col.GREEN,
+            Col.PURPLE,
+            Col.GOLD,
+            Col.ORANGE
+        ).map {
+            Color.name(it)
+        }
     }
 
-    var csvText: String? = null
     var chart: Component? = null
 
     override fun start() {
         root(rootId) {
-            p {
+            vPanel {
                 input(InputType.FILE) {
                     onChange {
-                        TODO("error")
                         val file = getElementD().files[0]
                         val fr = FileReader()
-                        fr.readAsText(file as Blob)
-                        csvText = fr.result as String
+                        fr.onload = {
+                            val text = it.target.asDynamic().result
 
+                            if (text != null) {
+                                chart?.let { it1 -> this@vPanel.remove(it1) }
+                                chart = null
+                                chart = this@vPanel.myChart(text)
+                            }
 
-                        chart?.let { it1 -> this@root.remove(it1) }
-                        chart = null
-
-                        if (csvText != null) {
-                            chart = this@root.myChart()
+                            Unit
                         }
+                        fr.readAsText(file)
                     }
                 }
             }
         }
-//        chart?.let { it1 -> this@root.remove(it1) }
-//        chart = null
 //
-//        if (csvText != null) {
-//            chart = this@root.myChart()
-//        }
     }
 
-    fun Container.myChart(): Component {
-        val read = csvReader().readAll(csvText!!)
+    fun Container.myChart(text: String): Component {
+        val read = csvReader().readAll(text)
         val transformed = read[0].mapIndexed { i,_ ->
             read.map { it[i] }
         }
         val labels = transformed[0].drop(1)
+        val dataBagTags = transformed.drop(1).map { it[0] }
         val dataBagList = transformed.drop(1).map {
             it.drop(1).map { it.toDoubleOrNull() ?: Double.NaN }
         }
+        transformed.forEach { println(it) }
 
-        val myDataSets =
+        val myDataSetsList =
+            dataBagList.mapIndexed { i, it ->
+                DataSets(
+                    cubicInterpolationMode = InterpolationMode.MONOTONE,
+                    data = it,
+                    fill = false,
+                    borderColor = listOf(chartColorList[i]),
+                    tension = 0.4,
+                    label = dataBagTags[i]
+                )
+            }
+
+        val zipped = dataBagList.map { labels zip it }
+        val combined = zipped.flatMapIndexed { i, l ->
+            zipped.mapIndexed { j, it ->
+                if (l == it) null
+                else (i to j) to (l to it)
+            }.filterNotNull()
+        }
+        val pointBagList = combined.map { (ij,lm) ->
+            val (i,j) = ij
+            val (l,m) = lm
+
+            val pointList = l.flatMap { (s1,u) ->
+                m.filter { (s2,_) -> s1 == s2 }.map { (_,v) -> u to v }
+            }.distinct().filter {
+                !it.first.isNaN() && !it.second.isNaN()
+            }.map {
+                object : ScatterDataPoint {
+                    override var x: Number = it.first
+                    override var y: Number = it.second
+                }
+            }
+
             DataSets(
-                cubicInterpolationMode = InterpolationMode.MONOTONE,
-                data = dataBagList,
-                fill = false,
-                borderColor = dataBagList.mapIndexed { i, _ ->
-                    chartColorList[i]
+                data = pointList,
+                pointBorderColor = listOf(i,j).map { chartColorList[it] },
+                label = listOf(i,j).joinToString("-") {
+                    dataBagTags[it]
                 },
-                tension = 0.4
+                pointBackgroundColor = listOf(i,j).map { chartColorList[it] }
             )
+        }
 
 
-        return p {
+        return vPanel {
             chart(
                 Configuration(
                     ChartType.LINE,
-                    listOf(myDataSets),
+                    myDataSetsList,
                     labels
                 )
             )
+            pointBagList.forEach {
+                chart(
+                    Configuration(
+                        ChartType.SCATTER,
+                        listOf(it)
+                    )
+                )
+            }
         }
     }
 
